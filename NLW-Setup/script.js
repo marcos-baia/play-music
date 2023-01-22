6e91e771227dbad55d897ef563e8c9e4f278e7ab
MARCOS BAIA <marcosjsbaia@yahoo.com.br>
2023-01-22 12:03:02 -0300

    função de adicionar dia

--- NLW-Setup/script.js
+++ NLW-Setup/script.js
@@ -1,9 +1,25 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
+button.addEventListener("click", add)
+
+function add(){
+  const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if(dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  alert('Adicionado com sucesso')
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+// const data = {
+//   run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
