5aacd8424023b478345d359d6ab3f0d409622e45
MARCOS BAIA <marcosjsbaia@yahoo.com.br>
2023-01-22 12:25:39 -0300

    recuperar os dados

--- NLW-Setup/script.js
+++ NLW-Setup/script.js
@@ -24,6 +24,6 @@ function save(){
 // const data = {
 //   run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
 // }
-
-// nlwSetup.setData(data)
-// nlwSetup.load()
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits"))
+nlwSetup.setData(data)
+nlwSetup.load()
