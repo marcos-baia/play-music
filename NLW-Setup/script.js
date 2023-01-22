185a37c2e5903a4e856ce044ecbdec03dc8c67e6
MARCOS BAIA <marcosjsbaia@yahoo.com.br>
2023-01-22 12:20:42 -0300

    salvamento dos dados

--- NLW-Setup/script.js
+++ NLW-Setup/script.js
@@ -3,6 +3,7 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
 button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add(){
   const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
@@ -17,6 +18,9 @@ function add(){
   nlwSetup.addDay(today)
 }
 
+function save(){
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
 // const data = {
 //   run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
 // }
