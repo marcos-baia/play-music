0c98472c0eb19551ea72e077bde564284816d5ee
marcos-baia <marcosjsbaia@yahoo.com.br>
2023-04-14 00:06:37 -0300

    informações das musicas

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+// REFERENCES
 const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
 const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
 const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
 
-songName.innerText = 'Alive (It Feels Like)';
+// VARIABLES
+const Alive = {
+  songName : 'Alive (It Feels Like)',
+  artist : 'Alok',
+  file : 'Alive (It Feels Like)'
+};
+const BigJetPlane = {
+  songName : 'Big Jet Plane',
+  artist : 'Alok & Mathieu Koss',
+  file : 'Big Jet Plane'
+};
+const Favela = {
+  songName : 'Favela',
+  artist : 'Alok & Ina Wroldsen',
+  file : 'Favela'
+};
 let isPlaying = false;
+const playlist = [Alive, BigJetPlane, Favela];
+let index = 0;
 
+
+// FUNCTIONS
 function playSong(){
   play.querySelector('.bi').classList.remove('bi-play-circle-fill');
   play.querySelector('.bi').classList.add('bi-pause-circle-fill');
@@ -27,4 +51,38 @@ function playPauseDecider(){
   }
 }
 
+function initializeSong(){
+  cover.src = `images/${playlist[index].file}.jpg`;
+  song.src = `songs/${playlist[index].file}.mp3`;
+  songName.innerText = playlist[index].songName;
+  bandName.innerText = playlist[index].artist;
+}
+
+function previousSong(){
+  if(index === 0){
+    index = playlist.length - 1;
+  }else{
+    index -= 1
+  }
+  initializeSong();
+  playSong();
+}
+
+function nextSong(){
+  if(index === playlist.length - 1){
+    index = 0;
+  }else{
+    index += 1
+  }
+  initializeSong();
+  playSong();
+}
+
+//EXECUTIONS OF FUNCTIONS
+initializeSong();
+
+
+// ADD EVENT
 play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
